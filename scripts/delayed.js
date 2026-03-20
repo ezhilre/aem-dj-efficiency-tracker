33e4d46ff4809d28df7d1399b9e3988dbaea3ee6
Ezhilarasu R <ezhilarasur@adobe.com>
2026-03-20 19:25:58 +0530

    devc

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,7 @@
 // add delayed functionality here
+(function loadAnalytics() {
+  const script = document.createElement('script');
+  script.src = 'https://www.googletagmanager.com';
+  script.async = true;
+  document.head.appendChild(script);
+})();
